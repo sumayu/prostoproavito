c8eeeadb7beee4a98d7eb361c22ed199e102a556
Костя <konstantinsavin930@gmail.com>
2025-12-24 04:12:33 +0300

    prerender html for seo

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import prerender from 'vite-plugin-prerender'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    prerender({
+      routes: ['/'], // только главная
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
