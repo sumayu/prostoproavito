e89ebc5aa8229cccaff7b550a4c5c9fde2f9af05
Костя <konstantinsavin930@gmail.com>
2025-12-24 04:17:13 +0300

    hotfix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import prerender from 'vite-plugin-prerender'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    prerender({
-      routes: ['/'], // только главная
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
